fee634f37083cc172ab52befedcd3925a8757514
Devan Skeem <devanskeem@gmail.com>
2019-05-20 13:51:56 -0600

    finished

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-
+import store, {ADD_INGREDIENT} from '../../store'
 class Ingredients extends Component {
   constructor(props) {
     super(props);
+    const reduxState = store.getState()
+    console.log(reduxState)
     this.state = {
-      ingredients: [],
+      ingredients: reduxState.ingredients,
       input: ""
     };
+  }
+
+  componentDidMount(){
+    
+    store.subscribe(() => {
+      const reduxState = store.getState()
+      this.setState({
+        ingredients: reduxState.ingredients
+      })
+    })
   }
   handleChange(val) {
     this.setState({
@@ -15,12 +27,16 @@ class Ingredients extends Component {
     });
   }
   addIngredient() {
-    // Send data to Redux state
+    store.dispatch({
+      type: ADD_INGREDIENT,
+      payload: this.state.input
+    })
     this.setState({
       input: ""
     });
   }
   render() {
+    console.log(this.state.ingredients)
     const ingredients = this.state.ingredients.map((ingredient, i) => {
       return <li key={i}>{ingredient}</li>;
     });
